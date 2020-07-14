8e6b6184730612a2410882cd5ed2f5f2982d7c83
sn1ch <sn1ch@list.ru>
2020-07-14 21:05:36 +0300

    db_url add

--- bot/main.py
+++ bot/main.py
@@ -5,12 +5,14 @@ from tortoise import Tortoise
 from bot.db.models import Botbakuadmin_subcategory, Botbakuadmin_product, Botbakuadmin_eat, Botbakuadmin_beer, \
     Botbakuadmin_category, Botbakuadmin_text, Botbakuadmin_tguser, Botbakuadmin_casino, Botbakuadmin_casinouser
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from bot.config import TOKEN
+from bot.config import TOKEN, DATABASE_URL
 
 logging.basicConfig(level=logging.INFO)
 loop = asyncio.get_event_loop()
 loop.run_until_complete(
     Tortoise.init(db_url="postgres://admin:admin@localhost/test2", modules={"models": ["__main__"]}))
+
+loop.run_until_complete(Tortoise.init(db_url=DATABASE_URL, modules={"models": ["__main__"]}))
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage, loop=loop)
